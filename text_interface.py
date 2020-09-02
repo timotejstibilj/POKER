2c9fdbda4e844a1615a02167cd3648e8e71e8e72
Timotej Stibilj <timotej.stibilj@gmail.com>
2020-09-03 00:10:00 +0200

    Dodal črte za lažji pregled

--- text_interface.py
+++ text_interface.py
@@ -1,6 +1,7 @@
 from model import Igralec, Igra
 
-pomoc = "Ukazi: stanje, klici, povisaj, odstopi, check"
+pomoc = """Vpisati moraš ukaz: stanje, klici, povisaj, odstopi, check.
+Če želiš višati moraš dopisati še vrednost."""
 
 janez = Igralec("janez")
 igra = Igra(janez)
@@ -9,10 +10,12 @@ igra.nova_runda()
 
 def odigraj_krog():
     while not igra.runda.stave_so_poravnane():
-        tekst = input("> ")
+        print("============================================================= \n")
+        tekst = input("> Prosim za ukaz. \n")
         tekst = tekst.split()
         ukaz = tekst[0]
         vrednost = tekst[1] if len(tekst) == 2 else None
+        igra.stanje()
         if ukaz == "klici":
             igra.klici()
         elif ukaz == "povisaj":
@@ -28,18 +31,39 @@ def odigraj_krog():
 
 
 def deli_in_igraj(stevilo_kart):
+    igra.stanje()
     if igra.runda.imamo_predcasnega_zmagovalca():
-        return
+        return "Zmagovalec je častitljivi{}.".format(igra.runda.kdo_je_v_igri())
     igra.runda.deck.deli_karto(igra.runda.miza, stevilo_kart)
     odigraj_krog()
 
 
+def zakljucek_igre():
+    igra.runda.pripni_kombinacije(igra.runda.miza)
+    igra.runda.razdeli_pot()
+    igra.stanje()
+    print("Runda se je zaključila.")
+    print("=============================================================")
+    print("\n")
+
+
 while True:
-    tekst = input("Začni rundo? ")
+    tekst = input("Začni novo rundo? \n")
     if tekst != "ja":
+        print("Kaj potem počneš tukaj?")
         break
     igra.nova_runda()
+    print("============================================================= \n")
+    print("Si v preflop-u.")
     deli_in_igraj(0)
+    print("============================================================= \n")
+    print("Si v flop-u.")
     deli_in_igraj(3)
+    print("============================================================= \n")
+    print("Si na turn-u.")
     deli_in_igraj(1)
+    print("============================================================= \n")
+    print("Si na river-ju.")
     deli_in_igraj(1)
+    print("============================================================= \n")
+    zakljucek_igre()
